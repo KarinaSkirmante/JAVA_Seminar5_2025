a1ad431a9020443d8e73d0e2851b86419bc94d5c
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-14 11:43:17 +0200

    Edit: added controller for CRUD product - get one product by id

--- src/main/java/lv/venta/controller/ProductCRUDController.java
+++ src/main/java/lv/venta/controller/ProductCRUDController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lv.venta.model.Product;
 import lv.venta.service.IProductCRUDService;
@@ -32,7 +33,25 @@ public class ProductCRUDController {
 			return "show-error";
 		}
 	}
-	//kas ir iegūti no servisa
-	//parādīt html lapu (izmantojam jau eksistējošō show-multiple-products.htmlt)
-	//nostestēt kontrolieri
+	
+	@GetMapping("/one") //localhost:8080/product/crud/one?id=1
+public String getControllerGetOneProductById(@RequestParam(name = "id") long id, Model model) {
+		try {
+			Product productFound = prodService.retrieveById(id);
+			model.addAttribute("package", productFound);
+			return "show-one-product";
+		} catch (Exception e) {
+			model.addAttribute("package", e.getMessage());
+			return "show-error";
+		}
+	}
+	
+	
+	
+	
+	
+	//localhost:8080/product/crud/all/1
+	
+	
+	
 }
