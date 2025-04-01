d8ad2deffaef3079afc437eeb439452a7b76363b
KarinaSkirmante <karina.krinkele@gmail.com>
2025-04-01 11:53:16 +0300

    Edit: changed FilteringController to restController

--- src/main/java/lv/venta/controller/ProductFiltAndCalcController.java
+++ src/main/java/lv/venta/controller/ProductFiltAndCalcController.java
@@ -3,16 +3,19 @@ package lv.venta.controller;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import lv.venta.model.Product;
 import lv.venta.service.IProductFilterAndCalcService;
 
-@Controller
+@RestController
 @RequestMapping("/product/process")
 public class ProductFiltAndCalcController {
 	
@@ -20,16 +23,19 @@ public class ProductFiltAndCalcController {
 	private IProductFilterAndCalcService prodService;
 	
 	@GetMapping("/price/{threshold}")//localhost:8080/product/process/price/2.77
-	public String getControllerProductPriceLessThan
-	(@PathVariable(name ="threshold") float threshold, Model model)
+	public ResponseEntity<?> getControllerProductPriceLessThan
+	(@PathVariable(name ="threshold") float threshold)
 	{
 		try {
 			ArrayList<Product> filteredProducts = prodService.getAllProductsWherePriceLessThan(threshold);
-			model.addAttribute("package", filteredProducts);
-			return "show-multiple-products";
+			ResponseEntity<ArrayList<Product>> response 
+			= new ResponseEntity<ArrayList<Product>>(filteredProducts, HttpStatus.OK);
+			return response;		
+			
+			
 		} catch (Exception e) {
-			model.addAttribute("package", e.getMessage());
-			return "show-error";
+			ResponseEntity<String> response = new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+			return response;
 		}
 		
 		
